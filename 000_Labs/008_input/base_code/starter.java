f64adbbbe54a17b9320ee343f54fe06335b6baeb
AndrewDba <andrewdavoodi555@gmail.com>
2026-09-09 20:54:47 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -8,7 +8,27 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		// the command window when you compile and run this program. 
+		Scanner sc = new Scanner(System.in);
+	    
+		
+		System.out.println("What is your first name");
+		
+		String age = sc.nextLine();
+		System.out.println("What is your age");
+		
+		String month = sc.nextLine();
+		System.out.println("What month were you born? (1-12)");
+		
+        String day = sc.nextLine();
+		System.out.println("What day were you born? (1-31)");
+        
+
+		String year = sc.nextLine();
+		System.out.println("What year were you born? (1-12)");
+
+		String buckfity = sc.nextLine();
+		System.out.println("How much is a buck fifty? (0.0)");
+
 	}
 }
